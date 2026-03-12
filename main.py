b8adbf7ffdb8c176e9d566ec5d860411321aa9bc
maru37320 <maru37320@gmail.com>
2026-03-12 11:06:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import random as ran
 st.title('안녕하세요 이마루입니다 반갑습니다')
 for i in range(5):
-  a = ran(1,3)
-  if a % 2 == 0:
+  if i % 2 == 0:
     st.write('감사합니다')
   else:
     st.write('하하호호')
